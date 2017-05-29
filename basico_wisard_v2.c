9754ff74b46034c2ab17a586a8fab4090001619b
Juliana Fernandes <julianaf.piaz@gmail.com>
2017-05-29 18:13:34 -0300

    Add files via upload

--- basico_wisard_v2.c
+++ basico_wisard_v2.c
@@ -59,13 +59,15 @@ Associar o vetor embaralhado de indices às entradas da RAM. CAda ram pega uma q
 */
 
 #include "basico_wisard.h"
+
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <math.h>
 
 #define OK					0
 
 
+/*Essa função está funfando! lol*/
 void 
 CriarVetorEmbaralhado (ull *vetorEmbaralhado)
 {
@@ -73,7 +75,7 @@ CriarVetorEmbaralhado (ull *vetorEmbaralhado)
  
    // Criando o Vetor Ordenado.
    printf ("Vetor Ordenado: \n");
-	for (indiceVetor = 0; indiceVetor < TAMANHO_VETOR; indiceVetor++)
+	for (indiceVetor = 0; indiceVetor < TAMANHO_VETOR_ENTRADA; indiceVetor++)
 	{	
 		vetorEmbaralhado [indiceVetor] = indiceVetor;
     	printf ("%llu ", vetorEmbaralhado [indiceVetor]);
@@ -87,10 +89,10 @@ CriarVetorEmbaralhado (ull *vetorEmbaralhado)
 	{
 	
     	// Variável que recebe o indice Aleatório.
-    	indiceAleatorio1 = rand() % TAMANHO_VETOR;
+    	indiceAleatorio1 = rand() % TAMANHO_VETOR_ENTRADA;
 
         // Variável que recebe o valor apontado pelo indice Aleatório.
-        indiceAleatorio2 = rand() %TAMANHO_VETOR;
+        indiceAleatorio2 = rand() %TAMANHO_VETOR_ENTRADA;
 	    
 	    // Colocando o Valor de outro índice dentro do primeiro.
 	    valorIndice = vetorEmbaralhado [indiceAleatorio1];
@@ -102,7 +104,35 @@ CriarVetorEmbaralhado (ull *vetorEmbaralhado)
 	}
 }
 
-/* Gera a Tabela correspondente à cada elemento de 'vetorUPLAS'. Com TODAS as possibilidades de score!
+
+/*__________________________________________________________________________________________*/
+
+
+void valorDiscriminadores (ull **vetorUplas, ull **vetorDiscriminadores)
+{
+    ull vetorUplas [QUANTIDADE_UPLAS] [TAMANHO_UPLA];
+
+    // cada vetor associado a CADA Upla tem tamanho = 2 ^ TAMANHO_UPLA 
+    ull vetorDiscriminadores [QUANTIDADE_UPLAS] [ pow(2, TAMANHO_UPLA) ];
+    
+    int indiceUplas, indiceBit, contadorDePadrao;
+    
+    for (indiceUplas = 0; indiceVetor < QUANTIDADE_UPLAS; indiceVetor++)
+    {
+        // Essa linha descobre o valor do índice ao qual deve ser somado 1 no vetorDiscriminador.
+        contadorDePadrao = BinToDec (vetorUplas [indiceVetor]);
+        
+        for (indiceBit = 0; indiceBit < TAMANHO_UPLA; indiceBit++)
+        {   
+            
+        }
+
+    }
+    
+    
+}
+
+/* Gera cada elemento de 'vetorUPLAS'. No caso, uplas com NBits. Com TODAS as possibilidades de score!
 Essa função devolve uma Tabela com todos os Scores possíveis.
 
 Upla É um vetor de elementos. =)
@@ -111,55 +141,41 @@ tá.
 vetorUplas é um vetor de vetores. que contém todas as Uplas! lol
 
 */
-
-void GerarTabelaIndices (ull *vetorUplas, ull *discriminadores)
+void GerarUPLAS (ull *vetorEmbaralhado, ull **vetorUplas)
 {
-    ull vetorUplas [TAMANHO_VETOR] [TAMANHO_UPLA];
-    int indiceVetor;
-    
-    
-    for (indiceVetor = 0; indiceVetor < TAMANHO_VETOR; indiceVetor++)
+    int indiceUplas, indiceBit;
+
+   /* Vou TENTAR dividir em tamanhos de 8 bits ( TAMANHO_UPLA ).
+    MAS Tenho que dividir SEMPRE por um múltiplo do tamanho.
+    por isso, vou tentar gerar uma entrada múltipĺa de 8.*/
+
+    // Primeiro for percorre até o tamanho total do vetor de entrada (vetorEmbaralhado) pulando pelo comprimento total de cada upla.
+    for (indiceUplas = 0; indiceUplas < TAMANHO_VETOR_ENTRADA; indiceUplas += (TAMANHO_UPLA-1))
     {
-        /* o Elemento apresentado abaixo é uma UPLA. lol
-         Ou seja, essa é a entrada correta e necessária. */        
-        BinToDec (vetorUplas [indiceVetor]);
-
-    
+        // Segundo for percorre até o tamanho total da upla. 0-7!  E preenche a posição com o bit correnpondente do vetorEmbralhado.
+        for (indiceBit = 0; indiceBit < TAMANHO_UPLA; indiceBit++)
+        {
+            vetorUplas [indiceUplas] [indiceBit] = vetorEmbaralhado [indiceBit];
+        }    
     }
-    
-    
-}
 
-/* Dividir o 'vetorEmbaralhado' de Tamanho = TAMANHO_VETOR
-*/
-void GerarUPLAS (ull *vetorEmbaralhado, ull *vetorUplas)
-{
-    
 }
 
 /* Essa Função converte um número binário para um número decimal.*/
 ull BinToDec (ull upla[TAMANHO_UPLA])
 {
-    int indiceVetor, indicePeso, indiceInterno;
+    int indiceVetor;
     ull valorDecimal = 0;
-    ull potencia = 2;
     
-        /* para o vetor, os menores índices representam os maiores valores. [0] = 2^(TAMANHO_VETOR)
+        /* para o vetor, os menores índices representam os maiores valores. [0] = 2^(TAMANHO_VETOR_ENTRADA)
          então temos dois índices para que a transição aconteça de forma correta.*/
-    for (indicePeso = TAMANHO_VETOR, indiceVetor = 0; indiceVetor < TAMANHO_VETOR; indiceVetor++, indicePeso)
+    for (indiceVetor = 0; indiceVetor < TAMANHO_UPLA; indiceVetor++)
     {
-        //  queo fazer: valorDecimal += valorBinario [indiceVetor]*(2^indicePeso);
+        //  quero fazer: valorDecimal += valorBinario [indiceVetor]*(2^TAMANHO_UPLA);
 
-        for (indiceInterno = 1; indiceInterno < indicePeso; indiceInterno++)
-        {
-            potencia *= 2;
-            // Aqui pode existir um printf ("%llu ", potencia); 
-            // com o valor atual da variável.
-        }
-
-        valorDecimal += valorBinario [indiceVetor]*potencia;
+        valorDecimal += valorBinario [indiceVetor] * ( pow(2, TAMANHO_UPLA) );
     }
-        
+
     return valorDecimal;
 }
 
@@ -174,13 +190,13 @@ int main ()
     int semente = VALOR_SEMENTE;
    	srand (semente);
 
-	ull vetorEmbaralhado [TAMANHO_VETOR];
+	ull vetorEmbaralhado [TAMANHO_VETOR_ENTRADA];
 
 	CriarVetorEmbaralhado (vetorEmbaralhado);
 
 	printf ("\n\n");
 	
-	for (indiceVetor = 0; indiceVetor < TAMANHO_VETOR; indiceVetor++)
+	for (indiceVetor = 0; indiceVetor < TAMANHO_VETOR_ENTRADA; indiceVetor++)
 	{	
         printf ("%llu ", vetorEmbaralhado[indiceVetor]);
 	}
